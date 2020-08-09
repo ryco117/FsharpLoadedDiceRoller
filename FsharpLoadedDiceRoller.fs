621575d8b7e565ac297a83f5f938c399ccfe5395
Ryan Andersen <ryco117@gmail.com>
2020-08-08 23:49:08 -0700

    Add project files.

---
+++ FsharpLoadedDiceRoller.fs
@@ -0,0 +1,69 @@
+﻿(*
+This file is part of FsharpLoadedDiceRoller
+
+FsharpLoadedDiceRoller is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FsharpLoadedDiceRoller is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FsharpLoadedDiceRoller. If not, see <https://www.gnu.org/licenses/>.
+*)
+(*
+Original paper detailing algorithm for The Fast Loaded Dice Roller (FLDR)
+https://arxiv.org/pdf/2003.03830.pdf
+
+Official C and Python implementations
+https://github.com/probcomp/fast-loaded-dice-roller
+*)
+
+namespace FsharpLoadedDiceRoller
+
+type FairCoin =
+    abstract Flip: unit -> bool
+
+type FLDRoller(fairCoin: FairCoin, dist: int[]) =
+    let n = dist.Length
+    let m = Array.fold (fun acc x -> acc + x) 0 dist
+    let k =
+        let rec f x i =
+            if x = 1 then i else f (x >>> 1) (i + 1) in
+        let k' = f m 0
+        if (1 <<< k') < m then k' + 1 else k'
+    let arr = Array.zeroCreate (n + 1)
+    do Array.iteri (fun i _ -> arr.[i] <- if i < n then dist.[i] else (1 <<< k) - m) arr
+    let h = Array.zeroCreate k
+    let H = Array.zeroCreate (n + 1)
+    do Array.iteri (fun i _ -> H.[i] <- Array.zeroCreate k) H
+    do for j = 0 to k - 1 do
+        let mutable d = 0
+        for i = 0 to n do
+            let w = (arr.[i] >>> (k - 1) - j) &&& 1
+            h.[j] <- h.[j] + w
+            if w > 0 then
+                H.[d].[j] <- i
+                d <- d + 1
+
+    member _.Sample() =
+        let mutable d = 0
+        let mutable c = 0
+        let rec looper () =
+            let b = fairCoin.Flip ()
+            d <- 2 * d + (if b then 0 else 1)
+            if d < h.[c] then
+                if H.[int d].[c] < n then
+                    H.[int d].[c]
+                else
+                    d <- 0
+                    c <- 0
+                    looper ()
+            else
+                d <- d - h.[c]
+                c <- c + 1
+                looper ()
+        looper ()
---
+++ Program.fs
@@ -0,0 +1,77 @@
+﻿let license = """
+FsharpLoadedDiceRoller - Sample executable for performing the novel Fast Loaded Dice Roller algorithm (https://arxiv.org/pdf/2003.03830.pdf)
+Copyright (C) 2020  Ryan Andersen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+open System
+open Argu
+
+open FsharpLoadedDiceRoller
+
+type Arguments =
+    | Rolls of rolls:uint32
+    | Verbose
+    | Histogram of histogram:bool
+    | Distribution of distribution:int list
+    | License
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Rolls _ -> "specify number of rolls to calculate."
+            | Verbose -> "specify to print result of roll."
+            | Histogram _ -> "specify whether to print end histogram (default: true)."
+            | Distribution _ -> "specify the loaded distribution to use."
+            | License -> "display license information about FsharpLoadedDiceRoller."
+            | _ -> raise (new Exception "incomplete interface!!")
+
+[<EntryPoint>]
+let main argv =
+    // Setup simple PRNG
+    let rand = new Random ()
+    let flipper = { new FairCoin with member _.Flip () = (rand.Next ()) % 2 = 0 }
+
+    // Parse command-line inputs
+    let parser =  ArgumentParser.Create<Arguments>(programName = "FsharpLoadedDiceRoller")
+    let args = parser.Parse (argv, raiseOnUsage=false)
+    if args.IsUsageRequested then printfn "%s" (parser.PrintUsage(programName = "FsharpLoadedDiceRoller")); exit 0
+    if args.Contains License then printfn "%s" license; exit 0
+    let dist =
+        if args.Contains Distribution then
+            Array.ofList (args.GetResult Distribution)
+        else
+            [|1; 3; 1; 10|]
+    let hist = Array.zeroCreate (dist.Length)
+    let N =
+        if args.Contains Rolls then
+            int (args.GetResult Rolls)
+        else
+            100_000
+    let verbose = args.Contains Verbose
+    let printHist = not (args.Contains Histogram) || args.GetResult Histogram
+
+    // Let 'er Roll!
+    let roller = new FLDRoller (flipper, dist)
+    for _ = 1 to N do
+        let s = roller.Sample ()
+        if verbose then printfn "%i" s
+        hist.[s] <- hist.[s] + 1
+
+    // Print histogram
+    if printHist then
+        printfn "Histogram results:"
+        printfn "%s" (Array.fold (fun acc e -> acc + (e.ToString ()) + "\t\t") "" hist)
+    0
